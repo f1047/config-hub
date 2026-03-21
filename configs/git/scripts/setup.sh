7ce47929642ccfee5475332d9613e9537a8df889
Hiroaki Matsushige (MagicPod) <14920359+f1047@users.noreply.github.com>
2026-03-21 18:52:09 +0900

    create local git config from template

--- configs/git/scripts/setup.sh
+++ configs/git/scripts/setup.sh
@@ -2,6 +2,7 @@
 set -eu
 
 project_root="$(git rev-parse --show-toplevel)"
+this_dir="$(cd "$(dirname "$0")"; pwd)"
 
 . "$project_root"/utils/header.sh
 
@@ -9,14 +10,13 @@ project_root="$(git rev-parse --show-toplevel)"
 git_local_config="$HOME"/.local/share/git/config
 if [ ! -e "$git_local_config" ]; then
    mkdir -p "$(dirname "$git_local_config")"
-   printf "$(header info) Configure git local config"
+   printf "$(header info) Configure git local config\n"
    printf "$(header prompt) Enter your user.name: "
    read _name
    printf "$(header prompt) Enter your user.email: "
    read _email
-   echo """[user]
-   name = $_name
-   email = $_email""" >> "$git_local_config"
+   sed -e "s/{{NAME}}/$_name/" -e "s/{{EMAIL}}/$_email/" \
+      "$this_dir"/local.config.template > "$git_local_config"
    printf "$(header info) Configured git local config:\n"
    cat "$git_local_config"
 fi
